636ecad27ce5c7fa4f965346d02fdf19aa69977d
Nicolas Freitas <nicolasfpdev@gmail.com>
2024-04-11 20:46:53 -0300

    feat: finalizando primeiro projeto

--- 1. Color Flipper/hex.js
+++ 1. Color Flipper/hex.js
@@ -6,10 +6,13 @@ btn.addEventListener("click", function () {
     let hexColor = '#'
 
     for (let i = 0; i < 6; i++) {
-        hexColor += hex[0]
+        hexColor += hex[getRandomNumber()]
     }
 
     document.body.style.backgroundColor = hexColor
     color.textContent = hexColor
 })
 
+function getRandomNumber() {
+    return Math.floor(Math.random() * hex.length)
+}
